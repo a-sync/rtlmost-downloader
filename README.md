1f95f0def13d713eec4e9f09aefc9f6470c2b7e8
smith <andras@sexybuddha.hu>
2018-10-11 22:56:07 +0200

    use hd source

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # rtlmost-downloader [![npm (scoped)](https://img.shields.io/npm/v/rtlmost-downloader.svg)](https://www.npmjs.com/package/rtlmost-downloader) 
 A videók letöltését teszi lehetővé a megújult rtlmost.hu oldalról, regisztráció és bejelentkezés nélkül.
-![rtlmost-downloader](https://user-images.githubusercontent.com/14183614/35911255-97e75de8-0bf9-11e8-8e73-9eb749521979.gif)
+![rtlmost-downloader](https://user-images.githubusercontent.com/14183614/46839788-eb01a000-cdbf-11e8-834b-649eff7da58d.gif)
 
 ## Indítás parancssori dialógussal
 
--- bin/rtlmost-downloader.js
+++ bin/rtlmost-downloader.js
@@ -1,27 +1,29 @@
 #!/usr/bin/env node
-process.title = 'rtlmost-downloader (@a-sync)';
+process.title = 'github.com/a-sync/rtlmost-downloader';
 
-const {setup, parser, downloader} = require('../index.js');
+const {setup, parser, downloader} = require('..');
 
 if (process.argv.length > 2) {
     setup.parseCmdArgs()
-    .then(download)
-    .catch(err => {
-        console.error(err.message);
-    });
+        .then(download)
+        .catch(err => {
+            console.error(err.message);
+        });
 } else {
     setup.showPrompts()
-    .then(download)
-    .catch(err => {
-        console.error(err.message);
-    });
+        .then(download)
+        .catch(err => {
+            console.error(err.message);
+        });
 }
 
 function download(params) {
     return parser.load(params.url).then(
         videoUrls => {
+            // Debug: console.debug(JSON.stringify(videoUrls, null, 2));
+
             if (Array.isArray(videoUrls) && videoUrls.length > 0) {
-                const targetUrl = findGoodTarget(videoUrls);
+                const targetUrl = bestGuess(videoUrls);
 
                 if (targetUrl || process.argv.length > 2) {
                     downloader.download(targetUrl, params.file);
@@ -39,13 +41,22 @@ function download(params) {
     });
 }
 
-function findGoodTarget(videoUrls) {
-    return videoUrls
+function bestGuess(videoUrls) {
+    const url = videoUrls
         .filter(url => {
             return (url.indexOf('_drmnp.ism/') === -1);
         })
         .find(url => {
             return (url.indexOf('_unpnp.ism/') !== -1);
         });
+
+    if (url && url.indexOf('.fr/v1/resource/s/') !== -1) {
+        const urlId = url.split('.fr/v1/resource/s/')[1].split('_unpnp.ism/')[0];
+        const videoId = urlId.split('/').pop();
+
+        return 'https://rtlhu.vod.6cloud.fr/' + urlId + '_unpnp.ism/' + videoId + '_.m3u8';
+    }
+
+    return url;
 }
 // TODO: try to download all other URLs if no good target can be found, or the first try fails
